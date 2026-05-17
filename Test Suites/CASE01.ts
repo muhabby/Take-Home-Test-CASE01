7b4b37f7c9e9a23bc9e8ac0d8f19dcde9e50e1bc
Muhabby Mulya <muhabbymulya94@gmail.com>
2026-05-17 23:06:25 +0700

    Rename Entry Point Test Suite

--- Test Suites/CASE01.ts
+++ Test Suites/CASE01.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>Test Suite CASE01</name>
+   <name>CASE01</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
